7c42fabcec49f063333966e3b222c0076eb60ebe
achrefabdennebi <achref.abdennebi@gmail.com>
2020-11-14 11:08:38 +0100

    feat: add post models

--- network/models.py
+++ network/models.py
@@ -1,6 +1,13 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.db.models.deletion import DO_NOTHING
 
 
 class User(AbstractUser):
     pass
+
+class Post(models.Model):
+    content = models.CharField(max_length=250)
+    created_by = models.ForeignKey(User,null=True, blank=True, on_delete=DO_NOTHING, related_name="users")
+    created_date = models.DateTimeField() 
+
