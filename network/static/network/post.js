e9f295cf4f9df1d7bb2a96fb8ae5ccf40605a45b
achrefabdennebi <achref.abdennebi@gmail.com>
2020-11-14 13:33:14 +0100

    feat: add post behavior front part

--- network/static/network/post.js
+++ network/static/network/post.js
@@ -1,5 +1,36 @@
-const sayHello = function (name) {
-    return `Hello ${name}`;
+document.addEventListener('DOMContentLoaded', function ()  {
+    // click post btn event
+    document.querySelector('#postBtn').addEventListener('click', () => savePost());
+});
+
+/** 
+ *  API: SAVE POST 
+ */
+function savePostApi(postData) {
+    // Post data of new data
+    fetch('/save', {
+        method: 'POST',
+        body: JSON.stringify(postData)
+    })
+    .then(response => response.json())
+    .then(() => resetPostField());
 }
 
-console.log(sayHello(`Achref`));
+/**
+ * Save post api
+ */
+function savePost() {
+    const postContent = document.getElementById(`postContent`).value;
+    const postData = {
+        content: postContent
+    }    
+    savePostApi(postData)
+}
+
+/**
+ * Update Textarea field aft
+ */
+function resetPostField () {
+    document.getElementById(`postContent`).value = ``;
+}
+
