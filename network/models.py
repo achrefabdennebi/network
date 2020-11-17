f1123b65bb821768a9c67695f2eb3bf0eb9edc9b
achref.abdennebi <achref.abdennebi.ext@suez.com>
2020-11-17 18:33:20 +0100

    feat: add followers models

--- network/models.py
+++ network/models.py
@@ -2,7 +2,6 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.db.models.deletion import DO_NOTHING
 
-
 class User(AbstractUser):
     pass
 
@@ -21,3 +20,12 @@ class Post(models.Model):
 
     def __str__(self):
         return f"{self.content[:25] } - {self.created_by} / ({self.created_date.strftime('%b %d %Y, %I:%M %p') })"
+
+class Follower(models.Model):
+    follower = models.ForeignKey(User,on_delete=models.CASCADE, related_name="following")
+    following = models.ForeignKey(User,on_delete=models.CASCADE, related_name="followers")
+    class Meta:
+        unique_together = ('follower', 'following')
+
+    def __str__(self):
+        return u'%s follows %s' % (self.follower, self.following)
