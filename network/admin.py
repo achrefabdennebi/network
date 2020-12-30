302a0bdcb18ec1dd7b55e2df3bea6955ea79555a
achrefabdennebi <achref.abdennebi@gmail.com>
2020-12-30 11:29:11 +0100

    feat: add likes admin site

--- network/admin.py
+++ network/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Post, User, Follower
+from .models import Post, User, Follower, Like
 
 # Register your models here.
 admin.site.register(Post)
 admin.site.register(User) 
-admin.site.register(Follower) 
+admin.site.register(Follower)
+admin.site.register(Like)
 
